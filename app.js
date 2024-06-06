ac927bbb8e4be55e47f743db358b0f24fd40baee
William Andry <williamandrysm98@gmail.com>
2024-06-05 19:18:14 -0600

    Utilizacion en el javaScript template string, else y live server

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-let numeroSecreto = 6 
+//Variables
+let numeroSecreto = 8; 
 let numeroUsuario = prompt("Me indicas un número por favor:");
 
 console.log(numeroUsuario);
 
+/*Este código realiza
+la comparación */
+
 if(numeroUsuario == numeroSecreto){
-    alert('Acertaste el número');
+    //Acertamos, fue verdadera la condicion
+    alert(`Acertaste, el número es: ${numeroUsuario}`);
+}else{
+    //La condición no se cumplió
+    alert('Lo siento, no acertaste el número');
 }

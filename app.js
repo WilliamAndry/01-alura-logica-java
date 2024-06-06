4d96e29b3ce2f74d2ff9abf64fcb0ea95cae7152
William Andry <williamandrysm98@gmail.com>
2024-06-05 19:55:21 -0600

    Se uso el while,contador y adaptacion de contador de vez y veces

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 //Variables
-let numeroSecreto = 8; 
-let numeroUsuario = prompt("Me indicas un número por favor:");
+let numeroSecreto = 4; 
+let numeroUsuario = 0;
+let intentos=1;
+let palabraVeces='vez';
 
-console.log(numeroUsuario);
+while(numeroUsuario != numeroSecreto){
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-/*Este código realiza
-la comparación */
-
-if(numeroUsuario == numeroSecreto){
-    //Acertamos, fue verdadera la condicion
-    alert(`Acertaste, el número es: ${numeroUsuario}`);
-}else{
-    //La condición no se cumplió
-    alert('Lo siento, no acertaste el número');
+    console.log(numeroUsuario);
+    if(numeroUsuario == numeroSecreto){
+        //Acertamos, fue verdadera la condicion
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en  ${intentos} ${palabraVeces}`);
+    }else{
+        if(numeroUsuario > numeroSecreto){
+            alert('El numero secreto es menor');
+        }else{
+            alert('el numero secreto es mayor');
+        }
+        intentos = intentos+1;
+        palabraVeces='veces';
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
 }

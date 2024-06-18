464fb3da05e818d801cd919f82623a7d39ebc104
William Andry <williamandrysm98@gmail.com>
2024-06-18 02:16:08 -0600

    Codigo finalizado con desafio

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/*
 //Variables
 let numeroSecreto = Math.floor(Math.random()*10)+1; 
 let numeroUsuario = 0;
@@ -34,3 +35,52 @@ while(numeroUsuario != numeroSecreto){
         }
     }
 }
+/*
+/* DESAFIO */
+/*HACER QUE EL CODIGO FUNCIONES CON LA CANTIDAD "n" QUE INGRESE EL USUARIO*/
+
+//FORMA DE WILLIAM ANDRY
+let numeroLimite=0;
+numeroLimite = prompt("Ingresa al número maximo que deseas adivinar");
+
+let intentosLimite=0;
+intentosLimite = prompt("¿En cuantos intentos cres encontrarlo?");
+alert(`Excelente!! tienes ${intentosLimite} intentos para encontrarlo`);
+
+let numeroSecreto = Math.floor(Math.random()*numeroLimite)+1; 
+let numeroUsuario = 0;
+let intentos=1;
+
+//let palabraVeces='vez';
+let maximosIntentos= intentosLimite;
+while(numeroUsuario != numeroSecreto){   
+    
+    numeroUsuario = parseInt(prompt(`Me indicas un número entre 1 y ${numeroLimite} por favor:`));
+
+    console.log(numeroSecreto);
+    console.log(typeof(numeroSecreto));
+    console.log(typeof(numeroUsuario));
+
+    if(numeroUsuario == numeroSecreto){
+        //Acertamos, fue verdadera la condicion
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en  ${intentos} ${intentos==1?'Vez':'Veces'}`);
+    }else{
+        if(numeroUsuario > numeroSecreto){
+            alert('El numero secreto es menor');
+        }else{
+            alert('el numero secreto es mayor');
+        }
+        //intentos = intentos+1;
+        intentos++;
+        
+        palabraVeces='veces';
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+        if(intentos > maximosIntentos){
+            alert(`Llegaste al número maximo de ${maximosIntentos} intentos`);
+            break;
+        }
+    }
+}
+
+
